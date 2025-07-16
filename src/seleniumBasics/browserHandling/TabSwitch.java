fcde318691dc4bb737e9dfc68bcd9e28480e216e
waghkomal <waghkomal43@gmail.com>
2025-07-16 20:44:02 +0530

    added String code

--- src/seleniumBasics/browserHandling/TabSwitch.java
+++ src/seleniumBasics/browserHandling/TabSwitch.java
@@ -2,4 +2,26 @@ package seleniumBasics.browserHandling;
 
 public class TabSwitch {
 
+
+	// Input - Komal Bhushan More 
+	// Output - More Bhushan Komal
+	
+	String name ="Komal Bhushan More";
+	String name2 = "";
+	
+	public static void main(String[] args) {
+		
+		String name ="Komal Bhushan More";
+		String name2 = "";
+		
+		String[] newname=  name.split(" ");
+		
+		for (int i=newname.length-1 ; i>=0;i--) {
+			System.out.print(newname[i]+" ");
+			
+		}
+		
+		
+		
+	}
 }
